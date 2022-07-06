583f2a75d992f818d7aff534cc28d9d8881cd6dc
Bruna Saraiva <102065727+bruna-saraiva@users.noreply.github.com>
2022-07-05 21:15:41 -0300

    Add files via upload

---
+++ struct.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  struct ficha                        //struct de nome ficha com só uma das características
+    {
+        char marca[30];
+    }carro[2];                          //variável, no caso um vetor, com 3(?) carros
+
+
+
+int main()
+{
+
+    char fiat[6]= "fiat",toyota[15]= "toyota";
+    int opcao_marca=0;
+
+    marca ();
+
+}
+int marca ()
+{
+
+
+    char fiat[6]= "fiat",toyota[15]= "toyota";
+    int opcao_marca=0;
+
+    printf("Selecione 1 para fiat e 2 para toyota");
+    scanf("%d",&opcao_marca);
+
+   switch(opcao_marca){
+    case 1:
+        for(int i=0;i<=6;i++)                  //loop que coloca um texto que está dentro de um vetor dentro da "posição" marca dentro do struct carro(nesse carro o primeiro do vetor)
+            carro[0].marca[i]=fiat[i];
+    break;
+    case 2:
+        for(int i=0;i<=6;i++)
+            carro[0].marca[i]= toyota[i];
+    break;
+    }
+    printf("%s",carro[0].marca);
+}
